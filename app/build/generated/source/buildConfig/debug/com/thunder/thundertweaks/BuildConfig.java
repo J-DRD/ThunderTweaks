321a3aaaaac91ba8748118c1801121eb1a4e5b02
JORDAN DYCKES <54044028+J-DRD@users.noreply.github.com>
2020-03-12 15:22:23 +0000

    Update dependency versions, fix some issues, more todo.

--- app/build/generated/source/buildConfig/debug/com/thunder/thundertweaks/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/thunder/thundertweaks/BuildConfig.java
@@ -5,9 +5,9 @@ package com.thunder.thundertweaks;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.thunder.thundertweaks";
+  public static final String APPLICATION_ID = "com.qr.thundertweaks";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1040;
-  public static final String VERSION_NAME = "1.0.4";
+  public static final int VERSION_CODE = 1060;
+  public static final String VERSION_NAME = "1.0.6";
 }
